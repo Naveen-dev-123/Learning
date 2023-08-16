ac4fde8ae894f840edd62a6d256b42e7473a5197
Naveen-dev-123 <forstudyingknowledge@gmail.com>
2023-08-17 00:06:04 +0530

    second

---
+++ Learning/second.sh
@@ -0,0 +1,23 @@
+function dis()
+{
+echo "Naveen"
+}
+
+function ph()
+{
+echo '1234'
+}
+select option in diplay_name phone
+do
+	case $option in 
+	diplay_name)
+		dis
+		echo "name called";;
+		
+		
+	phone) 
+		ph;;
+	esac
+done
+
+
